8f756089da360370936d007d271ce3dc1d73612a
Sai Gayathri Mantha <47597129+gayathrimantha@users.noreply.github.com>
2023-10-28 18:03:59 +0530

    Update mapPolyfill.js

--- mapPolyfill.js
+++ mapPolyfill.js
@@ -2,11 +2,14 @@
 
 //Creating a prototype with custom name
 Array.prototype.myMap = function(callback) {
+  //creating an empty array to return it as map returns a newArray
   let returnArray = [];
+  //Applying the callback logic to each item
   for(let i=0; i<this.length;i++){
     if(callback){
       returnArray.push(callback(this[i]))
     }
   }
+  //returning the array
   return returnArray;
 }
