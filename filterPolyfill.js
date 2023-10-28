60cc2c157bf1137e6667a2d8950cd883b18b0160
Sai Gayathri Mantha <47597129+gayathrimantha@users.noreply.github.com>
2023-10-28 18:19:14 +0530

    Update filterPolyfill.js

--- filterPolyfill.js
+++ filterPolyfill.js
@@ -3,5 +3,10 @@
 Array.prototype.myFilter = function(callback){
   let returnArray = [];
 
-  
+  for(let i=0; i<this.length; i++){
+    if(callback(this[i])){
+      returnArray.push(this[i])
+    }
+  }
+  return returnArray;
 }
